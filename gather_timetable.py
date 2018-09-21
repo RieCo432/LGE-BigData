6f870e62e37d443058a256ed71f10a531e928aae
Colin Ries <colin.ries@gmail.com>
2018-09-21 11:20:02 +0100

    pulled new timetables

--- gather_timetable.py
+++ gather_timetable.py
@@ -6,13 +6,15 @@ from shared_stuff import ClassTeacher, ClassClass
 
 driver = webdriver.PhantomJS()
 
-class_list = ["1B latine", "1B", "1C1", "1C2 latine", "1C2", "1D", "1E(F)", "1(E)F", "1D(G)", "1(D)G latine", "1(D)G",
-              "1G", "2A", "2A", "2A", "2B(C) latine", "2B(C)", "2(B)C", "2C latine", "2C", "2D(G)", "2(D)G", "2D(F)",
-              "2(D)F", "2E latine", "2E", "2G", "2G", "3B(C) latine", "3B(C)", "3(B)C latine", "3(B)C", "3C latine",
-              "3C", "3D latine", "3D", "3D(G)", "3(D)G", "3E(F) latine", "3E(F)", "3E(F) latine", "3(E)F",
-              "3E(G) latine", "3E(G)", "3(E)G", "IV.1", "41", "IV.2", "42", "43", "44", "45", "V.1", "51", "52", "53",
-              "54", "VI 1", "61", "62", "63", "64", "65", "71", "72", "73", "74", "75", "76", "CLIJA+", "1ère,2e", "2e",
-              "3ème", "DECHARGES", "DISPO", "Activités", "6e", "LHCE"]
+# class_list = ["1B latine", "1B", "1C1", "1C2 latine", "1C2", "1D", "1E(F)", "1(E)F", "1D(G)", "1(D)G latine", "1(D)G",
+#              "1G", "2A", "2A", "2A", "2B(C) latine", "2B(C)", "2(B)C", "2C latine", "2C", "2D(G)", "2(D)G", "2D(F)",
+#              "2(D)F", "2E latine", "2E", "2G", "2G", "3B(C) latine", "3B(C)", "3(B)C latine", "3(B)C", "3C latine",
+#              "3C", "3D latine", "3D", "3D(G)", "3(D)G", "3E(F) latine", "3E(F)", "3E(F) latine", "3(E)F",
+#              "3E(G) latine", "3E(G)", "3(E)G", "IV.1", "41", "IV.2", "42", "43", "44", "45", "V.1", "51", "52", "53",
+#              "54", "VI 1", "61", "62", "63", "64", "65", "71", "72", "73", "74", "75", "76", "CLIJA+", "1ère,2e", "2e",
+#              "3ème", "DECHARGES", "DISPO", "Activités", "6e", "LHCE"]
+
+class_list = []
 
 teacher_list = ClassTeacher.modify_teacher_list(ClassTeacher.original_teacher_list)
 
@@ -20,65 +22,77 @@ lessons_list = []
 
 classes_done = []
 
-for h in range(1, len(class_list) + 1):
+# for h in range(1, len(class_list) + 1):
+for h in range(1, 71 + 1):
 
-	if not class_list[h-1] in classes_done:
-	
-		driver.get("http://lge.lu/horaires/38/c/c" + str(h).zfill(5) + ".htm")
-		tree = html.fromstring(driver.page_source)
+    # if not class_list[h - 1] in classes_done:
+    if True:
+        driver.get("http://lge.lu/horaires/38/c/c" + str(h).zfill(5) + ".htm")
+        tree = html.fromstring(driver.page_source)
 
-		for i in range(2, 17, 2):  # cycle through times
-			for j in range(2, 7):  # cycle through days
+        detected_class = tree.xpath("/html/body/center/font[2]/b")[0].text.strip()
+        print(str(detected_class))
+        class_list.append(detected_class)
 
-				subject = tree.xpath(
-					"/html/body/center/table[1]/tbody/tr[" + str(i) + "]/td[" + str(j) + "]/table/tbody/tr/td[1]/font/b")
-				teacher = tree.xpath(
-					"/html/body/center/table[1]/tbody/tr[" + str(i) + "]/td[" + str(j) + "]/table/tbody/tr/td[2]/font/b")
-				room = tree.xpath(
-					"/html/body/center/table[1]/tbody/tr[" + str(i) + "]/td[" + str(j) + "]/table/tbody/tr/td[3]/font/b")
+        for i in range(2, 17, 2):  # cycle through times
+            for j in range(2, 7):  # cycle through days
 
-				for k in range(0, len(subject)):
+                subject = tree.xpath(
+                    "/html/body/center/table[1]/tbody/tr[" + str(i) + "]/td[" + str(
+                        j) + "]/table/tbody/tr/td[1]/font/b")
+                teacher = tree.xpath(
+                    "/html/body/center/table[1]/tbody/tr[" + str(i) + "]/td[" + str(
+                        j) + "]/table/tbody/tr/td[2]/font/b")
+                room = tree.xpath(
+                    "/html/body/center/table[1]/tbody/tr[" + str(i) + "]/td[" + str(
+                        j) + "]/table/tbody/tr/td[3]/font/b")
 
-					# if (subject[k] is not None) and (teacher[k] is not None) and (room[k] is not None):
-					# print(subject[0].text, teacher[0].text, room[0].text)
+                for k in range(0, len(subject)):
 
-					new_lesson = {
-						"day": j - 2,  # compensate for first column
-						"time": int(i / 2) - 1,  # compensate for empty rows
-						"class": class_list[h - 1],  # compensate for length=/=last index
-					}
+                    # if (subject[k] is not None) and (teacher[k] is not None) and (room[k] is not None):
+                    # print(subject[0].text, teacher[0].text, room[0].text)
 
-					try:
-						new_lesson["subject"] = subject[k].text.strip()
-					except IndexError:
-						new_lesson["subject"] = "None"
+                    new_lesson = {
+                        "day": j - 2,  # compensate for first column
+                        "time": int(i / 2) - 1,  # compensate for empty rows
+                        # "class": class_list[h - 1],  # compensate for length=/=last index
+                        "class": detected_class
+                    }
 
-					try:
-						shorted_teacher = teacher[k].text.split()
-						for teacher_name in teacher_list:
-							if teacher_name[0] == shorted_teacher[0] and teacher_name[1] == shorted_teacher[1]:
-								new_lesson["teacher"] = teacher_name
-					except IndexError:
-						new_lesson["teacher"] = "None"
+                    try:
+                        new_lesson["subject"] = subject[k].text.strip()
+                    except IndexError:
+                        new_lesson["subject"] = "None"
 
-					try:
-						new_lesson["room"] = room[k].text.strip()
-					except IndexError:
-						new_lesson["room"] = "None"
+                    try:
+                        shorted_teacher = teacher[k].text.split()
+                        for teacher_name in teacher_list:
+                            if teacher_name[0] == shorted_teacher[0] and teacher_name[1] == shorted_teacher[1]:
+                                new_lesson["teacher"] = teacher_name
+                    except IndexError:
+                        new_lesson["teacher"] = "None"
 
-					lessons_list.append(new_lesson)
+                    try:
+                        new_lesson["room"] = room[k].text.strip()
+                    except IndexError:
+                        new_lesson["room"] = "None"
 
-				if len(subject) == 0 and (int(i / 2) - 1 == 4 or int(i / 2) - 1 == 5):
-					new_lunch_break = {
-						"day": j - 2,  # compensate for first column
-						"time": int(i / 2) - 1,  # compensate for empty rows
-						"class": class_list[h - 1],  # compensate for length =/= last index
-						"subject": "SERVICE"
-					}
+                    lessons_list.append(new_lesson)
 
-					lessons_list.append(new_lunch_break)
-				
-		classes_done.append(class_list[h-1])
+                if len(subject) == 0 and (int(i / 2) - 1 == 4 or int(i / 2) - 1 == 5):
+                    new_lunch_break = {
+                        "day": j - 2,  # compensate for first column
+                        "time": int(i / 2) - 1,  # compensate for empty rows
+                        # "class": class_list[h - 1],  # compensate for length =/= last index
+                        "class": detected_class,
+                        "subject": "SERVICE"
+                    }
+
+                    lessons_list.append(new_lunch_break)
+
+    # classes_done.append(class_list[h - 1])
 
 with open("lessons_list.json", "w") as fout:
     json.dump(lessons_list, fout)
+with open("class_list.txt", "w") as fout:
+    fout.write(str(class_list))
